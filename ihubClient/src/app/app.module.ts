f437b2a63b823b4fab5ab119da5ba1e28f04a6f1
Pamod460 <pamodkaushika@gmail.com>
2022-10-21 20:56:10 +0530

    Create some componets
    login coponent,main window and item veiw

--- ihubClient/src/app/app.module.ts
+++ ihubClient/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemVeiwComponent } from './item-veiw/item-veiw.component';
+import { ItemVeiwComponent } from './veiws/main-window/item-veiw/item-veiw.component';
+import { LoginComponent } from './veiws/login/login.component';
+import { MainWindowComponent } from './veiws/main-window/main-window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemVeiwComponent
+    ItemVeiwComponent,
+    LoginComponent,
+    MainWindowComponent
   ],
   imports: [
     BrowserModule,
